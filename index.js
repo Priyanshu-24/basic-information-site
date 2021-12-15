a3d119a9a23d8ce5485f1106db9b8add1c756e1a
Priyanshu Pandey <priyanshu24pandey@gmail.com>
2021-12-15 18:51:37 +0530

    server setup done

--- index.js
+++ index.js
@@ -1,11 +1,66 @@
 const http = require("http");
+const fs = require("fs");
 
 const port = process.env.PORT || 8000;
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/html");
-    res.end("hello world");
+
+    if(req.url === "/") {
+
+        fs.readFile("index.html", (err, data) => {
+            if(err){
+                console.log(`Error: ${err}`);
+                return;
+            }
+            res.statusCode = 200;
+            res.setHeader("Content-Type", "text/html");
+            res.write(data);
+            return res.end();
+        });
+    }
+
+    else if(req.url === "/about") {
+
+        fs.readFile("about.html", (err, data) => {
+            if(err){
+                console.log(`Error: ${err}`);
+                return;
+            }
+            res.statusCode = 200;
+            res.setHeader("Content-Type", "text/html");
+            res.write(data);
+            return res.end();
+        });
+    }
+
+    else if(req.url === "/contact-me") {
+
+        fs.readFile("contact-me.html", (err, data) => {
+            if(err){
+                console.log(`Error: ${err}`);
+                return;
+            }
+            res.statusCode = 200;
+            res.setHeader("Content-Type", "text/html");
+            res.write(data);
+            return res.end();
+        });
+    }
+
+    else {
+
+        fs.readFile("404.html", (err, data) => {
+            if(err){
+                console.log(`Error: ${err}`);
+                return;
+            }
+            res.statusCode = 200;
+            res.setHeader("Content-Type", "text/html");
+            res.write(data);
+            return res.end();
+        });
+    }
+    
 });
 
 server.listen(port, () => {
